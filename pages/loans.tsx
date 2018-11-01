47d42f7571a8c50fc653743d4125fdbb4af80c13
3zk1m0 <sander.kallio@gmail.com>
2018-11-01 18:46:39 +0200

    Started loan site project

--- pages/loans.tsx
+++ pages/loans.tsx
@@ -1,7 +1,6 @@
 
 import React from 'react';
 
-
 import Appbar from "../src/components/Appbar"
 
 
--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import ListItemText from '@material-ui/core/ListItemText';
 
 import Link from 'next/link'
 
-const styles = {
+import GroupIcon from '@material-ui/icons/Group';
+import DevicesIcon from '@material-ui/icons/DevicesOther';
+
+const drawerWidth = 240;
+
+const styles = theme => ({
   root: {
-    flexGrow: 1,
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
   },
   grow: {
     flexGrow: 1,
@@ -19,34 +36,118 @@ const styles = {
     marginLeft: -12,
     marginRight: 20,
   },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+  },
+  toolbar: theme.mixins.toolbar,
+});
+
+class AppMenu extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        anchorEl: null,
+        loggedIn: false,
+      };
+  }
+  
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render(){
+    const { classes } = this.props;
+    const { anchorEl } = this.state;
+
+
+    let menuList;
+
+    if (this.state.loggedIn){
+      menuList = <div>
+        <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+        <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+      </div>
+    } else {
+      menuList = <div>
+        <MenuItem onClick={this.handleClose}>Login</MenuItem>
+      </div>
+    }
+
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+            <Typography color="inherit" noWrap>
+              Varaus järjestelmä
+            </Typography>
+            <div className={classes.grow} />
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu" aria-owns={anchorEl ? 'simple-menu' : null}
+              aria-haspopup="true"
+              onClick={this.handleClick}>
+              <AccountCircleIcon/>
+            </IconButton>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={this.handleClose}
+            >
+              {menuList}
+            </Menu>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.toolbar} />
+          <List>
+            <Link href='/equipments'>
+              <ListItem button key={'Equipment'}>
+                  <DevicesIcon/>
+                  <ListItemText primary={'Equipment'} />
+                </ListItem>
+            </Link>
+            <Link href='/users'>
+              <ListItem button key={'Users'}>
+                <GroupIcon/>
+                <ListItemText primary={'Users'} />
+              </ListItem>
+            </Link>
+            <Link href='/loans'>
+              <ListItem button key={'Loans'}>
+                <GroupIcon/>
+                <ListItemText primary={'Loans'} />
+              </ListItem>
+            </Link>
+          </List>
+        </Drawer>
+
+
+        
+      </div>
+    );
+  }
+}
+AppMenu.propTypes = {
+  classes: PropTypes.object.isRequired,
 };
 
 
-
-function ButtonAppBar(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="title" color="inherit" className={classes.grow}>
-            {props.page}
-          </Typography>
-          <Link href="/index">
-            <Button color="inherit" >Home</Button>
-          </Link>
-          <Link href="/countries">
-            <Button color="inherit" >Countries</Button>
-          </Link>
-          <Link href="/Clockpage">
-            <Button color="inherit" >Clock</Button>
-          </Link>
-          
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
-}
-
-
-export default withStyles(styles)(ButtonAppBar);
+export default withStyles(styles)(AppMenu);
